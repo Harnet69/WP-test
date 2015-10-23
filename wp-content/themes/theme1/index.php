9f63a7a0bfa77f9511d7a8b1cc628fc0a8f6b1e6
Harnet69 <motobycom@tut.by>
2015-10-23 15:26:34 +0300

    add sql base

---
+++ wp-content/themes/theme1/index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script src="js/jquery-1.10.1.min.js" type="text/javascript"></script>
+<script src="js/jqFancyTransitions.1.8.min.js" type="text/javascript"></script>
+<script>
+	$(document).ready( function(){
+		$('#slideshowHolder').jqFancyTransitions({ navigation: true, width: 594, height: 279 });
+	});
+	</script> 
+<title>Моя Тема WordPress для блога</title>
+</head>
+<body>
+	<div class="head-wrapper">
+		<div class="head">
+			<div class="head-logo"><img src="images/logo.jpg" alt="" /></div>
+			<div class="head-banner"><img src="images/728x90.jpg" alt="" /></div>
+		</div>
+	</div>
+	<div class="menu-wrapper">
+		<div class="menu-main">
+			<ul class="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Me</a></li>
+				<li><a href="#">Design Services</a></li>
+				<li><a href="#">Request Quote</a></li>
+				<li><a href="#">Advertise</a></li>
+				<li><a href="#">Contact Me</a></li>
+			</ul>
+			<ul class="ico-social">
+				<li><a href="#"><img src="images/ico-vk.png" alt=""></a></li>
+				<li><a href="#"><img src="images/ico-yt.png" alt=""/></a></li>
+				<li><a href="#"><img src="images/ico-f.png" alt=""/></a></li>
+				<li><a href="#"><img src="images/ico-tw.png" alt=""/></a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="content-wrapper">
+		<div class="content-main">
+			<div class="content">
+				<div id='slideshowHolder'>    
+						<img src="images/img1.jpg" alt='' />            
+						<img src="images/img1.jpg" alt='' />            
+						<img src="images/img1.jpg" alt='' />            
+					</div>
+				<div class="articles">
+					<div class="articles-gen-images">
+						<a href="#"><img src="Images\post-img1.jpg" alt="" /></a>
+					</div>
+					<div class="articles-head">
+						<span class="articles-date"><img src="Images\articles-author.jpg" alt="" /><span class="user-color">Admin</span> - Nov 28th, 2010</span>
+						<span class="articles-comments" ><img src="Images\articles-comment.jpg" alt=""><a href="#">10 Comments</a></span>
+					</div>
+					<div class="articles-body">
+						<h2><a href="#">Thanksgiving greeting card PSD</a></h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus. . </p>
+						<p><a href="#">Read More</a></p>
+					</div>
+				</div>
+				
+				<div class="articles">
+					<div class="articles-gen-images">
+						<a href="#"><img src="Images\post-img2.jpg" alt="" /></a>
+					</div>
+					<div class="articles-head">
+						<span class="articles-date"><img src="Images\articles-author.jpg" alt="" /><span class="user-color">Admin</span> - Nov 28th, 2010</span>
+						<span class="articles-comments" ><img src="Images\articles-comment.jpg" alt=""><a href="#">10 Comments</a></span>
+					</div>
+					<div class="articles-body">
+						<h2><a href="#">Thanksgiving greeting card PSD</a></h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus. . </p>
+						<p><a href="#">Read More</a></p>
+					</div>
+				</div>
+								<div class="articles">
+					<div class="articles-gen-images">
+						<a href="#"><img src="Images\post-img3.jpg" alt="" /></a>
+					</div>
+					<div class="articles-head">
+						<span class="articles-date"><img src="Images\articles-author.jpg" alt="" /><span class="user-color">Admin</span> - Nov 28th, 2010</span>
+						<span class="articles-comments" ><img src="Images\articles-comment.jpg" alt=""><a href="#">10 Comments</a></span>
+					</div>
+					<div class="articles-body">
+						<h2><a href="#">Thanksgiving greeting card PSD</a></h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus. . </p>
+						<p><a href="#">Read More</a></p>
+					</div>
+				</div>
+				
+				<div class="articles">
+					<div class="articles-gen-images">
+						<a href="#"><img src="Images\post-img4.jpg" alt="" /></a>
+					</div>
+					<div class="articles-head">
+						<span class="articles-date"><img src="Images\articles-author.jpg" alt="" /><span class="user-color">Admin</span> - Nov 28th, 2010</span>
+						<span class="articles-comments" ><img src="Images\articles-comment.jpg" alt=""><a href="#">10 Comments</a></span>
+					</div>
+					<div class="articles-body">
+						<h2><a href="#">Thanksgiving greeting card PSD</a></h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus. . </p>
+						<p><a href="#">Read More</a></p>
+					</div>
+				</div>
+				<div class="pager">
+					<a href="#">1</a>
+					<a href="#">2</a>
+					<a href="#">3</a>
+					<a href="#">4</a>
+					<span>of</span>
+					<a href="#">75</a>
+					<a href="#">Next</a>
+				
+				</div>
+			</div>
+			<div class="sidebar-main">
+				<div class="sidebar-widget">
+					<div class="search-main">
+						<form action="" method="GET">
+							<input class="search-txt" type="text" name="search" value="Search Blog" onfocus="if(this.value=='Search Blog')this.value=''" onblur="if(this.value=='')this.value='Search Blog'" />
+							<input class="search-img" type="image" src="Images\search-btn.jpg">
+						</form>
+					</div>
+					<div class="sidebar-widget">
+					<ul class="ads-main">
+						<li><a href="#"><img src="Images/ads1.jpg" alt=""/></a></li>				
+						<li><a href="#"><img src="Images/ads2.jpg" alt=""/></a></li>					
+						<li><a href="#"><img src="Images/ads3.jpg" alt=""/></a></li>					
+						<li><a href="#"><img src="Images/ads4.jpg" alt=""/></a></li>					
+					</ul>	
+					</div>
+					<div class="sidebar-widget">
+						<div class="sidebar-categories">
+							<h3>categories</h3>
+							<ul class="side-categories">
+								<li><a href="#">Backgrounds</a></li>
+								<li><a href="#">Brushes</a></li>
+								<li><a href="#">Design Articles</a></li>
+								<li><a href="#">Fonts</a></li>
+								<li><a href="#">Free PSD Files</a></li>
+								<li><a href="#">Icons</a></li>
+								<li><a href="#">Logos</a></li>
+								<li><a href="#">Patterns</a></li>
+								<li><a href="#">Textures</a></li>
+								<li><a href="#">Tools & Utilities</a></li> 
+							</ul>
+							<ul class="side-categories">
+								<li><a href="#">Backgrounds</a></li>
+								<li><a href="#">Brushes</a></li>
+								<li><a href="#">Design Articles</a></li>
+								<li><a href="#">Fonts</a></li>
+								<li><a href="#">Free PSD Files</a></li>
+								<li><a href="#">Icons</a></li>
+								<li><a href="#">Logos</a></li>
+								<li><a href="#">Patterns</a></li>
+								<li><a href="#">Textures</a></li>
+								<li><a href="#">Tools & Utilities </a></li>
+							</ul>
+							</div>
+					</div>
+					<div class="sidebar-widget">
+					<div class="sidebar-categories">
+						<h3>Latest posts</h3>
+						<p class="latest-post">
+							<img src="Images/latest-post1.jpg" alt="">
+							<a href="#">Request a quote form PSD in 3 colors</a>
+						</p>
+						<p class="latest-post">
+							<img src="Images/latest-post1.jpg" alt="">
+							<a href="#">Thanksgiving greeting card PSD</a>
+						</p>
+						<p class="latest-post">
+							<img src="Images/latest-post1.jpg" alt="">
+							<a href="#">Vintage business card PSD (front&back)</a>
+						</p>
+						<p class="latest-post">
+							<img src="Images/latest-post1.jpg" alt="">
+							<a href="#">Free xHTML/CSS website template</a>
+						</p>
+						<p class="latest-post">
+							<img src="Images/latest-post1.jpg" alt="">
+							<a href="#">fruit illustration, fresh lemon PSD</a>
+						</p>
+						<p class="latest-post">
+							<img src="Images/latest-post1.jpg" alt="">
+							<a href="#">Wooden speakers PSD & icons</a>
+						</p>
+					</div>
+					</div>
+					<div class="sidebar-widget">
+						<div class="sidebar-facebook">
+							<img src="Images\sidebar-facebook.jpg" alt=""/>
+						</div>
+					</div>
+					
+					
+				</div>
+			</div>
+		
+		</div>
+	</div>
+	<div class="footer-wrapper">
+		<div class="footer-main">
+			<div class="footer-info">
+				<h3>About</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus odio, quis rutrum nibh. Etiam nec tellus malesuada odio tincidunt fringilla quis vitae nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla malesuada convallis. Morbi laoreet volutpat rhoncus.</p>
+			</div>
+			<div class="footer-info">
+				<h3>Browse</h3>
+				<ul>
+					<li><a href="#"></a>Home</li>
+					<li><a href="#"></a>About Me</li>
+					<li><a href="#"></a>Design Services</li>
+					<li><a href="#"></a>Request Quote</li>
+					<li><a href="#"></a>Advertise</li>
+					<li><a href="#"></a>Contact Me</li>
+				</ul>
+			</div>
+			<div class="footer-info">
+				<h3>Write of us</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus odio, quis rutrum nibh. Etiam nec tellus malesuada odio tincidunt fringilla quis vitae nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla malesuada convallis.</p>
+				<p><a href="#">Contact Us Now</a><p>
+			</div>
+		</div>
+	</div>
+	<div class="footer-copy">
+		<p class="copy">Copyright © 2010 All Rights Reserved</p>
+		<p class="by-st">Designed by <a href="#">Harnet69</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Powered by <a href="#">Wordpress</a></p>
+	</div>
+</body>
+</html>
